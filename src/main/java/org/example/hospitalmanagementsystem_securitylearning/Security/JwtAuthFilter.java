ade8336c83e545eae2e0ae39150b14e1f086c261
Nandini390 <Shara.nandini123@gmail.com>
2026-02-19 14:22:04 +0530

    Added Exception Handler

--- src/main/java/org/example/hospitalmanagementsystem_securitylearning/Security/JwtAuthFilter.java
+++ src/main/java/org/example/hospitalmanagementsystem_securitylearning/Security/JwtAuthFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -23,22 +24,28 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
     private final AuthUtil authUtil;
 
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-         log.info("incoming request:{}",request.getRequestURI());
-         final String requestTokenHeader=request.getHeader("Authorization");
-         if(requestTokenHeader==null || !requestTokenHeader.startsWith("Bearer ")){
-             filterChain.doFilter(request,response);
-             return;
-         }
-         String token=requestTokenHeader.substring(7).trim();
-         String userName=authUtil.getUsernameFromToken(token);
+        try {
+            log.info("incoming request:{}",request.getRequestURI());
+            final String requestTokenHeader=request.getHeader("Authorization");
+            if(requestTokenHeader==null || !requestTokenHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request,response);
+                return;
+            }
+            String token=requestTokenHeader.substring(7).trim();
+            String userName=authUtil.getUsernameFromToken(token);
 
-         if(userName!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-             User user=userRepository.findByUsername(userName).orElseThrow();
-             UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
-             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-         }
-         filterChain.doFilter(request,response);
+            if(userName!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+                User user=userRepository.findByUsername(userName).orElseThrow();
+                UsernamePasswordAuthenticationToken authentication= new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request,response);
+        }catch(Exception ex){
+             handlerExceptionResolver.resolveException(request,response,null,ex);
+        }
     }
 }
---
+++ src/main/java/org/example/hospitalmanagementsystem_securitylearning/errors/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package org.example.hospitalmanagementsystem_securitylearning.errors;
+
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.management.JMException;
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ApiError> handleUserNameNotFoundException(UsernameNotFoundException ex){
+        ApiError apiError=new ApiError("User not found with username: "+ex.getMessage(),HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiError,apiError.getStatusCode());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException ex){
+        ApiError apiError=new ApiError("Authentication Failed "+ex.getMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException ex){
+        ApiError apiError=new ApiError("Invalid JWT Token "+ex.getMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException ex){
+        ApiError apiError=new ApiError("Access Denied: Insufficient Permissions"+ex.getMessage(),HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(apiError,HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGenericException(Exception ex){
+        ApiError apiError=new ApiError("An unexpected Error occured: "+ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
